7d16aa1a0f1ff37a3eb48a8a1c81868a9189b114
LauElToro <LautyfigueroaLau@gmail.com>
2026-04-19 14:09:06 -0300

    feature: KYC

--- prisma/migrations/20260419170000_user_perfil_intereses_quiero/migration.sql
+++ prisma/migrations/20260419170000_user_perfil_intereses_quiero/migration.sql
@@ -1,2 +1,3 @@
 -- Palabras clave "Lo que quiero" para priorizar coincidencias y mostrar en perfil
-ALTER TABLE "UserPerfilMercado" ADD COLUMN "interesesQuiero" TEXT[] NOT NULL DEFAULT '{}';
+-- IF NOT EXISTS: compatible con columnas ya creadas por ensureSchema / entornos previos
+ALTER TABLE "UserPerfilMercado" ADD COLUMN IF NOT EXISTS "interesesQuiero" TEXT[] NOT NULL DEFAULT '{}';
